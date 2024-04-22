54450ab73e549e29a1400418f0629f00a2864c20
Hunde-D <hunddh2025@gmail.com>
2024-04-23 01:44:14 +0300

    add random color

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 // import { useState } from "react";
 
 import Accordion from "./components/accordion/Accordion";
+import RandomColor from "./components/random-color/RandomColor";
 
 function App() {
   return (
     <div className="App">
       <Accordion />
+      <RandomColor />
     </div>
   );
 }
