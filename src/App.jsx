1240f36fcb17e89caed8589701bcc540fc2e7996
Hunde-D <hunddh2025@gmail.com>
2024-04-22 22:06:43 +0300

    setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+// import { useState } from "react";
+
+import Accordion from "./components/accordion/Accordion";
+
+function App() {
+  return (
+    <div className="App">
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
