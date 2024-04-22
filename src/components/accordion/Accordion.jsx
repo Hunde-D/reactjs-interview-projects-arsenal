ce1cf069a76cfa009edce33a78956fdc52711a11
Hunde-D <hunddh2025@gmail.com>
2024-04-22 22:12:33 +0300

    accordion component

---
+++ src/components/accordion/Accordion.jsx
@@ -0,0 +1,63 @@
+// TODO : single selection
+// TODO : multiple selection
+import { useState } from "react";
+import data from "./data";
+import "./style.css";
+export default function Accordion() {
+  const [selected, setSelected] = useState([]);
+  const [multiSelected, setMultiSelected] = useState(false);
+
+  function handleSingleSelection(id) {
+    if (multiSelected) {
+      setSelected((prevSelection) => {
+        if (prevSelection.includes(id)) {
+          return prevSelection.filter((i) => i !== id);
+        } else {
+          return [...prevSelection, id];
+        }
+      });
+    } else {
+      setSelected(selected[0] === id ? [null] : [id]);
+      console.log(selected[0]);
+    }
+  }
+  function handleMultiSelection() {
+    console.log(selected[0]);
+    if (multiSelected) setSelected([selected[selected.length - 1]]);
+
+    setMultiSelected((prevSelection) => !prevSelection);
+    // console.log(multiSelected);
+
+    // setSelected(id === selected ? null : id);
+  }
+  return (
+    <div className="wrapper">
+      <button onClick={handleMultiSelection}>Toggle multi selection</button>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="accordion-item"
+                onClick={() => handleSingleSelection(item.id)}
+              >
+                <div className="head">
+                  <h3>{item.question}</h3>
+                  <span>+</span>
+                </div>
+                {selected.map((i, index) => {
+                  return i === item.id ? (
+                    <p key={index}>{item.answer}</p>
+                  ) : null;
+                })}
+              </div>
+            );
+          })
+        ) : (
+          <div>No data found</div>
+        )}
+      </div>
+    </div>
+  );
+}
